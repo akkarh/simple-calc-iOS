a67e235dba9febff958e80e8273a002322f00772
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-01-28 14:50:42 -0800

    Implemented basic math operations

--- simple-calc/simple-calc/ViewController.swift
+++ simple-calc/simple-calc/ViewController.swift
@@ -19,7 +19,68 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var prev = 0.0
+    var curr = 0.0
+    var opSelected = false
+    var operation = ""
+    var newOp = false
+    
+    
+    @IBOutlet weak var output: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if (opSelected || newOp) {
+            output.text = ""
+            opSelected = false
+            newOp = false
+        }
+        output.text = output.text! + String(sender.tag - 1)
+    }
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        output.text = ""
+        clear()
+    }
+    
+    @IBAction func operation(_ sender: UIButton) {
+        if !opSelected {
+            prev = Double(output.text!)!
+            output.text = ""
+            opSelected = true
+            operation = sender.titleLabel!.text!
+        }
+    }
+    
+    
+    @IBAction func equalsPressed(_ sender: UIButton) {
+        if output.text != "" {
+            curr = Double(output.text!)!
+            switch operation {
+            case "+" :
+                output.text = String(prev + curr)
+            case "-" :
+                output.text = String(prev - curr)
+            case "x" :
+                output.text = String(prev * curr)
+            case "/" :
+                output.text = String(prev / curr)
+            case "%" :
+                output.text = String(prev.truncatingRemainder(dividingBy: curr))
+            default :
+                output.text = "Error!"
+            }
+        }
+        clear()
+    }
+    
+    func clear() {
+        prev = 0.0
+        curr = 0.0
+        opSelected = false
+        operation = ""
+        newOp = true
+    }
+    
 }
 
